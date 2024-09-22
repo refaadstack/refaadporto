dccf777fcced679117d38c6c2786d905b4c0dc53
refaadstack <refaad16@gmail.com>
2024-09-23 03:09:47 +0700

    Revert "add: schedule"
    This reverts commit 2b22019813f7925727725159cada94954edf2682.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\ScheduleServiceProvider::class,
     Barryvdh\DomPDF\ServiceProvider::class,
 ];
--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,13 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+$schedule->call(function () {
+    Artisan::call('route:list');
+})->daily();
