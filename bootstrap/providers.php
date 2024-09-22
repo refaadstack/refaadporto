2b22019813f7925727725159cada94954edf2682
refaadstack <refaad16@gmail.com>
2024-09-23 02:48:50 +0700

    add: schedule

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\ScheduleServiceProvider::class,
     Barryvdh\DomPDF\ServiceProvider::class,
 ];
