69df646a60c6bd6db9f48fa65bcfd49794c74f9b
refaadstack <refaad16@gmail.com>
2024-09-25 14:38:40 +0700

    fix: redudance on sitemap.xml

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -7,8 +7,9 @@ use Spatie\Sitemap\SitemapGenerator;
 use Spatie\Sitemap\Tags\Url;
 use App\Models\Portfolio;
 use App\Models\Blog;
+use Illuminate\Support\Facades\Log;
 
-class GenerateSitemapCommand extends Command
+class GenerateSitemap extends Command
 {
     protected $signature = 'sitemap:generate';
 
@@ -16,7 +17,7 @@ class GenerateSitemapCommand extends Command
 
     public function handle()
     {
-        $sitemap = SitemapGenerator::create(config('app.url'))
+            $sitemap = SitemapGenerator::create(config('app.url'))
             ->hasCrawled(function (Url $url) {
                 if ($url->segment(1) === '') {
                     $url->setPriority(1.0);
@@ -30,24 +31,33 @@ class GenerateSitemapCommand extends Command
             })
             ->getSitemap();
 
-        // Add static pages
-        $sitemap->add(Url::create('/')->setPriority(1.0));
-        $sitemap->add(Url::create('/portofolios')->setPriority(0.8));
-        $sitemap->add(Url::create('/blog')->setPriority(0.8));
-        $sitemap->add(Url::create('/#about')->setPriority(0.5));
-        $sitemap->add(Url::create('/#contact')->setPriority(0.5));
+            // Add static pages
+            $sitemap->add(Url::create('/')->setPriority(1.0));
+            $sitemap->add(Url::create('/portofolios')->setPriority(0.8));
+            $sitemap->add(Url::create('/blog')->setPriority(0.8));
+            $sitemap->add(Url::create('/#about')->setPriority(0.5));
+            $sitemap->add(Url::create('/#contact')->setPriority(0.5));
+            $sitemap->add(Url::create('/cv')->setPriority(0.5));
 
-        // Add dynamic portfolio pages
-        Portfolio::all()->each(function (Portfolio $portfolio) use ($sitemap) {
-            $sitemap->add(Url::create("/portofolios/{$portfolio->slug}")->setPriority(0.7));
-        });
+            // Add dynamic portfolio pages
+            Portfolio::all()->each(function (Portfolio $portfolio) use ($sitemap) {
+                $sitemap->add(
+                    Url::create("/portofolios/{$portfolio->slug}")
+                    ->setLastModificationDate($portfolio->updated_at)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                    ->setPriority(0.8));
+            });
 
-        // Add dynamic blog pages
-        Blog::all()->each(function (Blog $blog) use ($sitemap) {
-            $sitemap->add(Url::create("/blog/{$blog->slug}")->setPriority(0.7));
-        });
+            // Add dynamic blog pages
+            Blog::all()->each(function (Blog $blog) use ($sitemap) {
+                $sitemap->add(Url::create("/blog/{$blog->slug}")
+                ->setLastModificationDate($blog->updated_at)
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(0.7));
+            });
 
-        $sitemap->writeToFile(public_path('sitemap.xml'));
+            $sitemap->writeToFile(public_path('sitemap.xml'));
+            Log::channel('cronjob')->info('Cronjob sitemap berjalan pada: ' . now());
 
         $this->info('Sitemap generated successfully at: ' . public_path('sitemap.xml'));
     }
